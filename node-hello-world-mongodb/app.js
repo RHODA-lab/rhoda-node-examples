47f48b96103538ec99790378161fa623fc524f40
omveda <vedadashan@gmail.com>
2022-05-11 00:14:26 -0700

    list databases

--- node-hello-world-mongodb/app.js
+++ node-hello-world-mongodb/app.js
@@ -46,4 +46,14 @@ async function main() {
 
 main().catch(console.error);
 
-// Add functions that make DB calls here
+/**
+ * Print the names of all available databases
+ * @param {MongoClient} client A MongoClient that is connected to a cluster
+ */
+async function listDatabases(client) {
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
